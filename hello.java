fd96c71519f0c718c9123c4bbbdd07fb5f788891
V.Sindhujaa <vpsindhu@users.noreply.github.com>
2016-07-02 19:20:13 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,12 @@
+package hello;
+public class Hello {
+    public static void main(String[] args) {
+        
+        int i=0;
+        for(i=0;i<5;i++)
+        {
+            System.out.println("hello");
+        }
+    }
+    
+}
