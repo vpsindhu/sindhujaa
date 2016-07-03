afcaf5e7c47fc04dccc129241c3376f7323712f5
V.Sindhujaa <vpsindhu@users.noreply.github.com>
2016-07-03 18:53:23 +0530

    Create Ascii.java

---
+++ Ascii.java
@@ -0,0 +1,11 @@
+package ascii;
+public class Ascii {
+public static void main(String[] args) {
+        
+       for (int a=33;a<127;a++)
+       {
+System.out.println(a+ ":" + (char)a);
+    
+}
+    }
+}
