2666d57fcea011952f62310cf4e2fde3a5d96c94
V.Sindhujaa <vpsindhu@users.noreply.github.com>
2016-07-03 06:29:45 +0530

    Create oddno_intervals.java

---
+++ oddno_intervals.java
@@ -0,0 +1,21 @@
+package oddno_bw;
+import java.util.Scanner;
+public class Oddno_bw {
+    public static void main(String[] args) {
+            int low,high,c=0;
+        System.out.println("enter the low limit number");
+	Scanner s=new Scanner(System.in);
+        low=s.nextInt();
+        System.out.println("enter the high limit number");
+        high=s.nextInt();
+        System.out.println("odd numbers are");
+	for(int i=low;i<=high;i++)
+	{
+		if((i%2)!=0)
+                {
+		System.out.println(""+i);
+}
+    }
+    
+}
+}
