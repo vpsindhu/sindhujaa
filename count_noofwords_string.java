55f1936d1a0576a62f00bb346dcd4811a751a04f
V.Sindhujaa <vpsindhu@users.noreply.github.com>
2016-07-03 12:01:57 +0530

    Create count_noofwords_string.java

---
+++ count_noofwords_string.java
@@ -0,0 +1,11 @@
+package count_noofwords_string;
+public class Count_noofwords_string {
+    public static void main(String[] args) {
+        
+        String Count="hello hi";
+        String[] countingWord=Count.split("");
+        int n=countingWord.length;
+       System.out.println("count words ="+n);
+    
+}
+}
