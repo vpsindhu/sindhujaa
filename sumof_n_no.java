b89c0aec5ba9bd891ba965c54f183e233af30e6d
V.Sindhujaa <vpsindhu@users.noreply.github.com>
2016-07-09 14:40:14 +0530

    Create sumof_n_no.java

---
+++ sumof_n_no.java
@@ -0,0 +1,18 @@
+package sumof_n_no;
+import java.util.Scanner;
+public class Sumof_n_loop {
+
+    public static void main(String[] args) {
+        
+        int n,i,sum=0;
+        System.out.println("enter n value = ");
+        Scanner s=new Scanner(System.in);
+        n=s.nextInt();
+        for(i=1;i<=n;i++)
+        {
+            sum=sum+i;
+          
+        }
+        System.out.println("sum of n natural no = "+sum);
+    }
+    
