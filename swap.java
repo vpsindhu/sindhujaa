770ad3510ddc6c153d7e50fa72c03fe492545178
V.Sindhujaa <vpsindhu@users.noreply.github.com>
2016-07-09 14:43:51 +0530

    Create swap.java

---
+++ swap.java
@@ -0,0 +1,11 @@
+public class Swap {
+    public static void main(String[] args){
+    int a=9,b=11;
+    System.out.println("Before swapping: a is"+a+" b is"+b);
+    int temp;
+    temp=a;
+    a=b;
+    b=temp;
+    System.out.println("After swapping: a is"+a+"  b is"+b);
+    
+}
